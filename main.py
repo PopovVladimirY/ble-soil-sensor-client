f49fbe98829c7e9640fdc3579d918c8103da2686
Vladimir POPOV <vladimir.popov@free.fr>
2023-10-28 13:37:37 +0200

    using read instead of notification

--- main.py
+++ main.py
@@ -2,41 +2,51 @@ import asyncio
 import sys
 import numpy as np
 import datetime 
-import bleak
-#from bleak import BleakScanner, BleakClient, BleakGATTCharacteristic
+#import bleak
+from bleak import BleakScanner, BleakClient, BleakGATTCharacteristic
 
-def t_callback(sender: bleak.BleakGATTCharacteristic, data: bytearray):
-    fNotify: float = np.frombuffer(data, dtype=np.float32)
-    print(f"  Date/Time: {datetime.datetime.now()}")
-    print(f"Temperature: {fNotify[0]:.1f} ℃")
-    print(f"   Pressure: {fNotify[1]:.0f} Pa")
-    print(f"   Humidity: {fNotify[2]:.1f} %")
-    print(f"    Battery: {fNotify[3]:.2f} V")
-    print(f"       Soil: {fNotify[4]:.1f} %")
+def printData(data):
+    if data != None and len(data):
+        fNotify: float = np.frombuffer(data, dtype=np.float32)
+        print(f"  Date/Time: {datetime.datetime.now()}")
+        print(f"Temperature: {fNotify[0]:.1f} °C")
+        print(f"   Pressure: {fNotify[1]:.0f} Pa")
+        print(f"   Humidity: {fNotify[2]:.1f} %")
+        print(f"    Battery: {fNotify[3]:.2f} V")
+        print(f"       Soil: {fNotify[4]:.1f} %")
+
+def t_callback(sender: BleakGATTCharacteristic, data: bytearray):
+    printData(data)
 
 t_uuid: str = '9b5099ae-10f0-4a78-a8b7-eb086e3cb69b'
 
 async def main():
     while True:
         address = ['A0:B7:65:59:6F:BA']
-        '''        
+        '''                
+        devices = []
         try:
             # Scan is required to ensure stable reconnecion to device, as per Bleak manual
+            print("Scan for BLE devices")
             devices = await BleakScanner.discover()
+        except:
+            print("BLE scanner failed")
+            pass
+
+        if (len(devices)):
             for d in devices:
-    #            print(d)
+                print(d)
     #            print(d.name)
     #            print(d.address)
                 if d.name == 'W55':
                     print('Found W55!')
-                    address.append(d.address) #'A0:B7:65:59:6F:BA'
-        except:
-            pass
+#                    address.append(d.address) #'A0:B7:65:59:6F:BA'
+                            
         '''
         if len(address):
             for a in address:
                 try:
-                    ble = bleak.BleakClient(a)
+                    ble = BleakClient(a)
                     async with ble as client:
                         print("Connected")
 
@@ -44,9 +54,12 @@ async def main():
                             print("Connection failed")
                             raise "client not connected"
 
-                        await client.start_notify(t_uuid, t_callback)
-                        print("Wait for notification")
-                        await asyncio.sleep(5)
+                        await asyncio.sleep(1)
+                        data = await client.read_gatt_char(t_uuid)
+                        printData(data)
+#                        await client.start_notify(t_uuid, t_callback)
+#                        print("Wait for notification")
+#                        await asyncio.sleep(15)
             #                        print("Stop notification") 
             #                        await client.stop_notify(t_uuid)
                         print("Disconnect")
