cdfae4727f260feff53afac83838f6b3596de90e
Vladimir POPOV <vladimir.popov@free.fr>
2023-11-01 21:21:58 +0100

    adding second device to clean up bash script

--- ble_clean_cache.sh
+++ ble_clean_cache.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 bluetoothctl -- remove A0:B7:65:59:6F:BA
+bluetoothctl -- remove A0:B7:65:67:FE:26
 
 #prior to BlueZ 5.62 you also need to manually delete the GATT cache**
 sudo rm "/var/lib/bluetooth/DC:A6:32:54:0C:97/cache/A0:B7:65:59:6F:BA"
+sudo rm "/var/lib/bluetooth/DC:A6:32:54:0C:97/cache/A0:B7:65:67:FE:26"
